747725f025af880b1e55deb132187a13a19669b8
venturions <alex.eduardo10@hotmail.com>
2025-04-22 19:12:03 -0300

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import { app } from './app'
+import { env } from './env'
+
+app
+  .listen({
+    host: '0.0.0.0',
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('HTTP server running on http://localhost:3333')
+  })
+  .catch((err) => {
+    console.error('Error starting server:', err)
+    process.exit(1)
+  })
