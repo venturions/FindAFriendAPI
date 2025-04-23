16de3afe5103c3c476f28c2052d220c118257e81
venturions <alex.eduardo10@hotmail.com>
2025-04-23 09:51:05 -0300

    Criação da tabela Pet e Org e seus relacionamentos

---
+++ prisma/migrations/20250423124912_creation_of_the_pet_and_org_tables_and_their_relationships/migration.sql
@@ -0,0 +1,38 @@
+-- CreateTable
+CREATE TABLE "Org" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "whatsapp" TEXT NOT NULL,
+    "cep" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Org_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Pet" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "age" TEXT NOT NULL,
+    "size" TEXT NOT NULL,
+    "energyLevel" TEXT NOT NULL,
+    "independenceLevel" TEXT NOT NULL,
+    "environment" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "photos" TEXT[],
+    "adoptionRequirements" TEXT[],
+    "city" TEXT NOT NULL,
+    "orgId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Pet_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Org_email_key" ON "Org"("email");
+
+-- AddForeignKey
+ALTER TABLE "Pet" ADD CONSTRAINT "Pet_orgId_fkey" FOREIGN KEY ("orgId") REFERENCES "Org"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
